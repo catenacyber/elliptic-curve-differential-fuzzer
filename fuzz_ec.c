e8883b5dd9ea27a2711fcfae444676ce2106cf43
Philippe Antoine <contact@catenacyber.fr>
2018-08-21 13:13:38 +0200

    Dummy reduction of modules to test nettle

--- fuzz_ec.c
+++ fuzz_ec.c
@@ -48,7 +48,7 @@ size_t bitlenFromTlsId(uint16_t tlsid) {
     return 0;
 }
 
-#define NBMODULES 7
+#define NBMODULES 3
 //TODO integrate more modules
 void fuzzec_mbedtls_process(fuzzec_input_t * input, fuzzec_output_t * output);
 void fuzzec_libecc_process(fuzzec_input_t * input, fuzzec_output_t * output);
@@ -65,16 +65,6 @@ fuzzec_module_t modules[NBMODULES] = {
         NULL,
     },
     {
-        "libecc",
-        fuzzec_libecc_process,
-        NULL,
-    },
-    {
-        "libecc_montgomery",
-        fuzzec_libecc_montgomery_process,
-        NULL,
-    },
-    {
         "openssl",
         fuzzec_openssl_process,
         NULL,
@@ -82,16 +72,6 @@ fuzzec_module_t modules[NBMODULES] = {
     {
         "nettle",
         fuzzec_nettle_process,
-        NULL,
-    },
-    {
-        "gcrypt",
-        fuzzec_gcrypt_process,
-        fuzzec_gcrypt_init,
-    },
-    {
-        "cryptopp",
-        fuzzec_cryptopp_process,
         NULL,
     },
 };
